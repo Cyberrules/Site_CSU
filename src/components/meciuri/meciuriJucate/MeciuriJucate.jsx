80e30926ff577e185c3ed70b4c723c7eac0f9d45
LeonardFilip <150130127+LeonardFilip@users.noreply.github.com>
2023-12-28 23:02:15 +0200

    Optimizare MeciuriJucate si MeciuriViitoare

--- src/components/meciuri/meciuriJucate/MeciuriJucate.jsx
+++ src/components/meciuri/meciuriJucate/MeciuriJucate.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './MeciuriJucate.scss';
-import logoLiga from '../../assets/logoLiga.png';
+import React from "react";
+import "./MeciuriJucate.scss";
+import logoLiga from "../../assets/logoLiga.png";
 
 const MeciuriJucate = ({
   data,
@@ -13,41 +13,40 @@ const MeciuriJucate = ({
   numeEchipa2,
   scorEchipa2,
 }) => {
-  const raportMeci = 'Raport Meci';
-  
+  const raportMeci = "Raport Meci";
+
   return (
-    <div className='containerMeciuriJucate'>
-      <div className='cardMeciuriJucate'>
-        <div className='logoContainerJucate'>
-          <img src={logoLiga} alt='Logo' className='logoLigaJucate' />
-        </div>
-
-        <div className='infoContainerJucate'>
-          <p>{etapa}</p>
-          <p>{locatia}</p>
-        </div>
-
-        <div className='bara-verticalaJucate'></div>
-
-        <div className='infoDataJucate'>
-          <p>{data}</p>
-        </div>
-
-        <div className='elementeJucate'>
-          <div className='logoEchipeJucate'>
-            <p className='echipaGazdaJucate'>{numeEchipa1}</p>
-            <img src={logoEchipa1} alt='Logo' className='logoEchipeMeciJucate' />
+    <div className="container-meciuri-jucate">
+      <div className="card-meciuri-jucate">
+        <div className="wrapper-container-jucate">
+          <div className="logo-container-jucate">
+            <img src={logoLiga} alt="Logo" className="logo-liga-jucate" />
           </div>
-          <div className='scorMeciJucate'>{scorEchipa1}</div>
-          <div className='bara-orizontalaJucate'></div>
-          <div className='scorMeciJucate'>{scorEchipa2}</div>
-          <div className='logoEchipeJucate'>
-            <img src={logoEchipa2} alt='Logo' className='logoEchipeMeciJucate' />
-            <p className='echipaOaspeteJucate'>{numeEchipa2}</p>
+          <div className="info-container-jucate">
+            <p>{etapa}</p>
+            <p>{locatia}</p>
+          </div>
+          <div className="bara-verticala-jucate"></div>
+          <div className="info-data-jucate">
+            <p>{data}</p>
           </div>
         </div>
-
-        <button className='raportMeciJucate'>{raportMeci}</button>
+        <div className="elemente-jucate">
+          <div className="container-echipa-gazda">
+            <p className="nume-echipa-gazda">{numeEchipa1}</p>
+            <img src={logoEchipa1} alt="Logo" className="logo-echipa-gazda" />
+            <div className="scor-meci-gazda">{scorEchipa1}</div>
+          </div>
+          <div className="separator-scor-meci"></div>
+          <div className="container-echipa-oaspete">
+            <div className="scor-meci-oaspete">{scorEchipa2}</div>
+            <img src={logoEchipa2} alt="Logo" className="logo-echipa-oaspete" />
+            <p className="nume-echipa-oaspete">{numeEchipa2}</p>
+          </div>
+        </div>
+        <div className="container-raport-meci">
+          <button className="raport-meci-jucate">{raportMeci}</button>
+        </div>
       </div>
     </div>
   );
