e004a3240b58b66bde09c480a5af4d502b03ef64
Sorin <sorin.lungu292002@gmail.com>
2024-01-15 19:23:35 +0200

    Added CORS config for Admin controller

--- src/main/java/Cyberrules/demo/controller/AdminController.java
+++ src/main/java/Cyberrules/demo/controller/AdminController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
+@CrossOrigin(methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class AdminController {
     private final UserService userService;
 
